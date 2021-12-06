105ca2353cce1af29082655be3c6f2f1193c32e5
AlbertWebs <albertmuhatia@gmail.com>
2021-12-06 17:04:17 +0300

    Auth Unit

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+ 
 
 Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
