a771af9b8133b43adbc8646819727fe337b77081
AlbertWebs <albertmuhatia@gmail.com>
2021-12-08 16:33:42 +0300

    admins Theme

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,12 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
- 
-
-Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
 
 // Admin Routes
 Auth::routes();
